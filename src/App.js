55ff858904a425b4feab0ec7fd4c6560da16bd5f
Luis a.k.a. Wito <job.wito.uk@gmail.com>
2023-07-02 20:27:01 +0200

    Components added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import SobreMi from './components/SobreMi';
 import MiSitio from './components/MiSitio';
+import Horarios from './components/Horarios';
+import Tarifas from './components/Tarifas';
+import Celebraciones from './components/Celebraciones';
+import Calendario from './components/Calendario';
+import Galeria from './components/Galeria';
+import Contacto from './components/Contacto';
+
 //import React, { useState } from 'react'
 import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Proyectos from './components/Proyectos';
@@ -20,16 +27,22 @@ const App = () => {
             <ul className="navList">
               
               <li><Link to ="nosotros">Quiénes somos</Link></li>
-              
               <li ><Link to ="tarifas">Tarifas</Link></li>
               <li ><Link to ="horarios">Horarios disponibles</Link></li>
-              <li ><Link to ="tarifas">Celebraciones realizadas</Link></li>
+              <li ><Link to ="celebraciones">Celebraciones realizadas</Link></li>
+              <li ><Link to ="calendario">Calendario de eventos</Link></li>
+              <li ><Link to ="galeria">Galería</Link></li>
               <li ><Link to ="contacto">Contacto</Link></li>
             </ul>
           </nav>
           <Routes>
-            <Route path='/proyects' Component={Proyectos} />
-            <Route path='/myself' Component={SobreMi} />
+            <Route path='/nosotros' Component={SobreMi} />
+            <Route path='/tarifas' Component={Tarifas} />
+            <Route path='/horarios' Component={Horarios} />
+            <Route path='/celebraciones' Component={Celebraciones} />
+            <Route path='/calendario' Component={Calendario} />
+            <Route path='/galeria' Component={Galeria} />
+            <Route path='/contacto' Component={Contacto} />
             <Route path='/' Component={MiSitio} />
           </Routes>
         </Router>
