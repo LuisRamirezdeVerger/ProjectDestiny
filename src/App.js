f8680b1c323cb6ade3edcde52066e312beaee16f
Luis a.k.a. Wito <job.wito.uk@gmail.com>
2023-07-05 14:35:31 +0200

    gallery bugged added, need debug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+
 import SobreMi from './components/SobreMi';
 import MiSitio from './components/MiSitio';
 import Horarios from './components/Horarios';
@@ -14,12 +15,21 @@ import Proyectos from './components/Proyectos';
 
 const App = () => {
 
+  /*const images = [
+
+    {
+      url:  'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.vegaradio.com%2Fmedia%2Fk2%2Fitems%2Fcache%2F1d11e5ebeed3c42540f69555bbe34fb8_XL.jpg&tbnid=IHCVPr4A9LH-_M&vet=12ahUKEwiUrK2Mu_X_AhWEnCcCHYZRC-cQMygKegUIARDUAQ..i&imgrefurl=https%3A%2F%2Fwww.vegaradio.com%2Fnoticias%2Fllega-el-buenos-aires-blues-festival&docid=c_UwjO2vSFnnjM&w=1366&h=572&itg=1&q=el%20blues%20del%20boss&ved=2ahUKEwiUrK2Mu_X_AhWEnCcCHYZRC-cQMygKegUIARDUAQ',
+      title: "titulo A",
+    },
+  
+  ];*/
+
       return( 
-        
       <header className="header">
         <head>
         <title> Project Desiny</title>
         </head>
+        
         <Router>
           <nav className="navBar">
             <Link to="/"> Project Destiny </Link>
@@ -35,6 +45,8 @@ const App = () => {
               <li ><Link to ="contacto">Contacto</Link></li>
             </ul>
           </nav>
+          
+
           <Routes>
             <Route path='/nosotros' Component={SobreMi} />
             <Route path='/tarifas' Component={Tarifas} />
@@ -46,8 +58,11 @@ const App = () => {
             <Route path='/' Component={MiSitio} />
           </Routes>
         </Router>
+
+        
        
-     </header>   
+     </header>  
+      
      
   );
 }
--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,28 +1,46 @@
-import './Proyectos.css'
-import React from "react";
-import Counter from './Counter';
-import RatonEncima from './OnMouse';
-import Prompt from './Prompt'; 
+import './Galeria.css'
+import React, { useState } from "react";
 
-const Proyectos = () => {
+
+
+const Galeria = () => {
+    
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleImageClick = (image) => {
+        setSelectedImage(image)
+    };
+
+    const images = [
+
+        {
+          url:  'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.vegaradio.com%2Fmedia%2Fk2%2Fitems%2Fcache%2F1d11e5ebeed3c42540f69555bbe34fb8_XL.jpg&tbnid=IHCVPr4A9LH-_M&vet=12ahUKEwiUrK2Mu_X_AhWEnCcCHYZRC-cQMygKegUIARDUAQ..i&imgrefurl=https%3A%2F%2Fwww.vegaradio.com%2Fnoticias%2Fllega-el-buenos-aires-blues-festival&docid=c_UwjO2vSFnnjM&w=1366&h=572&itg=1&q=el%20blues%20del%20boss&ved=2ahUKEwiUrK2Mu_X_AhWEnCcCHYZRC-cQMygKegUIARDUAQ',
+          title: "titulo A",
+        },
+      
+      ];
+
 return(
-    <div style={{textAlign: 'center'}}>
-        <div style={{borderStyle: 'dotted'}}>
-            <h1>Contador</h1>
-            <Counter />
-        </div>
-        <br/>
-        <div>
-            <h1>On Mouse "event"</h1>
-            <RatonEncima />
-        </div>
-        <div>
-            <h1>Prompts</h1>
-            <Prompt />
-        </div>
-    </div>
-);
+   <div className="galeria">
+    
+        {images.map((image,index) => (
+            <img
+                key={index}
+                src={image.url}
+                alt={image.title}
+                onClick={() => handleImageClick(image)}
+            />
+        ))}
 
-}
+        {selectedImage && (
+            <div className="imagen-seleccionada">
+                <img 
+                src={selectedImage.url}
+                alt={selectedImage.title} />
+                <p>{selectedImage.title}</p>
+            </div>
+        )};
+   </div>
+);};
 
-export default Proyectos;
+export default Galeria;
