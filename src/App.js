3ff80c9a069e987f66abbbad24c079e093cae106
Luis a.k.a. Wito <job.wito.uk@gmail.com>
2023-07-07 14:06:11 +0200

    gallery on PC finished!, working on [onClick] events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import { Link, BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import SobreMi from './components/SobreMi';
 import MiSitio from './components/MiSitio';
 import Horarios from './components/Horarios';
@@ -9,62 +9,40 @@ import Calendario from './components/Calendario';
 import Galeria from './components/Galeria';
 import Contacto from './components/Contacto';
 
-//import React, { useState } from 'react'
-import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Proyectos from './components/Proyectos';
-
 const App = () => {
 
-  /*const images = [
+    return( 
+      <Router>  
+        <header className="header">
+          <title> Project Desiny</title>
+            <nav className="navBar">
+              <Link to="/"> Project Destiny </Link>
+              <ul className="navList">
+                <li><Link to ="nosotros">Quiénes somos</Link></li>
+                <li ><Link to ="tarifas">Tarifas</Link></li>
+                <li ><Link to ="horarios">Horarios disponibles</Link></li>
+                <li ><Link to ="celebraciones">Celebraciones realizadas</Link></li>
+                <li ><Link to ="calendario">Calendario de eventos</Link></li>
+                <li ><Link to ="galeria">Galería</Link></li>
+                <li ><Link to ="contacto">Contacto</Link></li>
+              </ul>
+            </nav>
+        </header>    
 
-    {
-      url:  'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.vegaradio.com%2Fmedia%2Fk2%2Fitems%2Fcache%2F1d11e5ebeed3c42540f69555bbe34fb8_XL.jpg&tbnid=IHCVPr4A9LH-_M&vet=12ahUKEwiUrK2Mu_X_AhWEnCcCHYZRC-cQMygKegUIARDUAQ..i&imgrefurl=https%3A%2F%2Fwww.vegaradio.com%2Fnoticias%2Fllega-el-buenos-aires-blues-festival&docid=c_UwjO2vSFnnjM&w=1366&h=572&itg=1&q=el%20blues%20del%20boss&ved=2ahUKEwiUrK2Mu_X_AhWEnCcCHYZRC-cQMygKegUIARDUAQ',
-      title: "titulo A",
-    },
-  
-  ];*/
+        <Outlet />
 
-      return( 
-      <header className="header">
-        <head>
-        <title> Project Desiny</title>
-        </head>
-        
-        <Router>
-          <nav className="navBar">
-            <Link to="/"> Project Destiny </Link>
-            
-            <ul className="navList">
-              
-              <li><Link to ="nosotros">Quiénes somos</Link></li>
-              <li ><Link to ="tarifas">Tarifas</Link></li>
-              <li ><Link to ="horarios">Horarios disponibles</Link></li>
-              <li ><Link to ="celebraciones">Celebraciones realizadas</Link></li>
-              <li ><Link to ="calendario">Calendario de eventos</Link></li>
-              <li ><Link to ="galeria">Galería</Link></li>
-              <li ><Link to ="contacto">Contacto</Link></li>
-            </ul>
-          </nav>
-          
-
-          <Routes>
-            <Route path='/nosotros' Component={SobreMi} />
-            <Route path='/tarifas' Component={Tarifas} />
-            <Route path='/horarios' Component={Horarios} />
-            <Route path='/celebraciones' Component={Celebraciones} />
-            <Route path='/calendario' Component={Calendario} />
-            <Route path='/galeria' Component={Galeria} />
-            <Route path='/contacto' Component={Contacto} />
-            <Route path='/' Component={MiSitio} />
-          </Routes>
-        </Router>
-
-        
-       
-     </header>  
-      
-     
-  );
-}
+        <Routes>
+              <Route path='/nosotros' element={<SobreMi/>} />
+              <Route path='/tarifas' element={<Tarifas/>} />
+              <Route path='/horarios' element={<Horarios/>} />
+              <Route path='/celebraciones' element={<Celebraciones/>} />
+              <Route path='/calendario' element={<Calendario/>} />
+              <Route path='/galeria' element={<Galeria />} />
+              <Route path='/contacto' element={<Contacto/>} />
+              <Route path='/' element={<MiSitio/>} />
+        </Routes>
+      </Router>
+   );
+};
 
 export default App;
--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,5 +1,6 @@
-import './Galeria.css'
+import './Galeria.css';
 import React, { useState } from "react";
+import test from '../images/fotoPruebaWeb.jpg';
 
 
 
@@ -7,28 +8,37 @@ const Galeria = () => {
     
     const [selectedImage, setSelectedImage] = useState(null);
 
+    //const handleImageClick = (image) => {
+    //    setSelectedImage(image);
+    //};
+
     const handleImageClick = (image) => {
-        setSelectedImage(image)
+        setSelectedImage((prevImage) => (prevImage === image ? null : image));
     };
 
     const images = [
 
         {
-          url:  'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.vegaradio.com%2Fmedia%2Fk2%2Fitems%2Fcache%2F1d11e5ebeed3c42540f69555bbe34fb8_XL.jpg&tbnid=IHCVPr4A9LH-_M&vet=12ahUKEwiUrK2Mu_X_AhWEnCcCHYZRC-cQMygKegUIARDUAQ..i&imgrefurl=https%3A%2F%2Fwww.vegaradio.com%2Fnoticias%2Fllega-el-buenos-aires-blues-festival&docid=c_UwjO2vSFnnjM&w=1366&h=572&itg=1&q=el%20blues%20del%20boss&ved=2ahUKEwiUrK2Mu_X_AhWEnCcCHYZRC-cQMygKegUIARDUAQ',
+          url:  test,
           title: "titulo A",
         },
+        {
+            url:  test,
+            title: "titulo b",
+          },
       
       ];
 
 return(
-   <div className="galeria">
-    
+   <div className="galeria" onClick={() => setSelectedImage(null)}>
+
         {images.map((image,index) => (
             <img
                 key={index}
                 src={image.url}
                 alt={image.title}
                 onClick={() => handleImageClick(image)}
+                
             />
         ))}
 
@@ -39,8 +49,9 @@ return(
                 alt={selectedImage.title} />
                 <p>{selectedImage.title}</p>
             </div>
-        )};
+        )}
    </div>
-);};
+    );
+};
 
 export default Galeria;
