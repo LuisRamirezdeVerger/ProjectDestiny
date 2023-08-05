8889cf54eff0bbc4838ea876b2479fbae3a5576c
Luis a.k.a. Wito <job.wito.uk@gmail.com>
2023-08-05 16:05:46 +0200

    Banner fixed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Horarios from './components/Horarios';
 import Tarifas from './components/Tarifas';
 import Galeria from './components/Galeria';
 import Contacto from './components/Contacto';
+import Logobanner from './images/banner/logoBanner.png';
 
 const App = () => {
 
@@ -15,11 +16,13 @@ const App = () => {
       <Router>  
         <header className="header">
             <nav className="navBar">
-              <Link to="/" className='tituloNavBar'></Link>
+              <Link to="/" className='tituloNavBar'>
+                <img src={Logobanner} className='logoTituloNavBar' alt='Logo'/>
+              </Link>
               <ul className="navList">
                 <li className='liApp'><Link to ="nosotros">Nosotros</Link></li>
                 {/* <li ><Link to ="tarifas">Tarifas</Link></li> */}
-                <li className='liApp'><Link to ="horarios">Reservas</Link></li>
+                <li className='liApp'><Link to ="horarios">Disponibilidad</Link></li>
                 {/*<li ><Link to ="celebraciones">Celebraciones realizadas</Link></li>*/}
                 {/* <li ><Link to ="calendario">Calendario de eventos</Link></li> */}
                 <li className='liApp'><Link to ="galeria">Galería</Link></li>
